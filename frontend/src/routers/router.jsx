e395053b07ffd608d627d8793a4c8a06d86cb226
Petar Mirchev <petar.g.mirchev@gmail.com>
2024-11-01 19:31:09 +0200

    fix fav-icon & add route for Login & register

--- frontend/src/routers/router.jsx
+++ frontend/src/routers/router.jsx
@@ -1,6 +1,8 @@
 import { createBrowserRouter } from "react-router-dom";
 import App from "../App";
 import Home from "../pages/home/Home";
+import Login from "../components/Login";
+import Register from "../components/Register";
 
 
 
@@ -20,7 +22,15 @@ const router = createBrowserRouter([
         {
             path: "/about",
             element: <div>About</div>
-        }
+        },
+        {
+          path: "/login",
+          element: <Login/>
+        },
+        {
+          path: "/register",
+          element: <Register/>
+        },
       ]
     },
   ]);
