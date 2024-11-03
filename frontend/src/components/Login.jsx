9c27a6c6e649cab69939444a27ef5741078c7a8d
Petar Mirchev <petar.g.mirchev@gmail.com>
2024-11-03 15:58:53 +0200

    add register form

--- frontend/src/components/Login.jsx
+++ frontend/src/components/Login.jsx
@@ -1,29 +1,46 @@
 import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
 import { FaGoogle } from "react-icons/fa";
-
+import { useForm } from "react-hook-form";
 
 
 
 const Login = () => {
-
   const [message, setMassage] = useState("");
+
+  const { register, handleSubmit, watch, formState: { errors } } = useForm();
+  const onSubmit = data => console.log(data); //send data from form
+
+
+  const handleGoogleSignIn = async () => {
+    try {
+        // await ....;
+        alert("Login successful!");
+        navigate("/")
+    } catch (error) {
+        alert("Google sign in failed!") 
+        console.error(error)
+    }
+  }
+
 
   return (
     <div className='h-[calc(100vh-120px)] flex justify-center items-center'>
       <div className='w-full max-w-sm mx-auto bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4'>
         <h2 className='text-xl font-semibold mb-4'>Please Login</h2>
 
-        <form onSubmit={null}>
+        <form onSubmit={handleSubmit(onSubmit)}>
             <div className='mb-4'>
               <label htmlFor="email" className='block text-grey-700 text-sm font-bold mb-2'>Email</label>
-              <input type="email" name='email' id='email' placeholder='Email Address' 
+              <input {...register("email", { required: true })}
+                type="email" name='email' id='email' placeholder='Email Address' 
                 className='shadow appearance-none border rounded w-full py-2 px-3 leading-tight focus:outline-none focus:shadow'
               />
             </div>
             <div className='mb-4'>
               <label className='block text-gray-700 text-sm font-bold mb-2' htmlFor="password">Password</label>
-              <input type="password" name="password" id="password" placeholder='Password'
+              <input {...register("password", { required: true })}
+                type="password" name="password" id="password" placeholder='Password'
                 className='shadow appearance-none border rounded w-full py-2 px-3 leading-tight focus:outline-none focus:shadow'
               />
             </div>
@@ -40,7 +57,7 @@ const Login = () => {
         {/* google sign in */}
         <div className='mt-4'>
           <button 
-            onClick={null}
+            onClick={handleGoogleSignIn}
             className='w-full flex flex-wrap gap-1 items-center justify-center bg-secondary hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none'>
             <FaGoogle  className='mr-2'/>
                 Sign in with Google
