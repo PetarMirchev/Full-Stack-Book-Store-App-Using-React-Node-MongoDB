5365f8e0df6ca66152675396c7e53dadf5bf00d3
Petar Mirchev <petar.g.mirchev@gmail.com>
2024-12-21 16:41:20 +0200

    add AuthContext for User

--- frontend/src/context/AuthContext.jsx
+++ frontend/src/context/AuthContext.jsx
@@ -1,3 +1,59 @@
-import { createContext } from "react";
+import { createContext, useContext, useEffect, useState } from "react";
+import { auth } from "../firebase/firebase.config";
+import { createUserWithEmailAndPassword, GoogleAuthProvider, onAuthStateChanged, signInWithEmailAndPassword, signInWithPopup, signOut } from "firebase/auth";
+
 
 const AuthContext = createContext();
+export const useAuth = () => {
+    return useContext(AuthContext);
+};
+
+
+// authProvider
+export const AuthProvider = ({children}) => {
+    const [currentUser, setCurrentUser] = useState(null);
+    const [loading, setLoading] = useState(true);
+
+    // register a user
+    const registerUser = async (email, password) => {
+        return await createUserWithEmailAndPassword(auth, email, password);
+    };
+
+    // login the user
+    const loginUser = async (email, password) => {
+       return await signInWithEmailAndPassword(auth, email, password);
+    };
+
+    // login whit Google
+    const signInWithGoogle = async () => {
+        const googleProvider = new GoogleAuthProvider();
+        return await signInWithPopup(auth, googleProvider);
+    };
+
+    // logout the user
+    const logout = async () => {
+        return signOut(auth);
+    };
+
+
+    // manage user
+    useEffect(() => {
+        // TO DO
+        return ;
+    }, []);
+
+    const value = {
+        currentUser,
+        loading,
+        registerUser,
+        loginUser,
+        signInWithGoogle,
+        logout
+    }
+
+    return (
+        <AuthContext.Provider value={value}>
+            {children}
+        </AuthContext.Provider>
+    )
+};
